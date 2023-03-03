6777a062222975d21afdf6ed824ef17d1597cb5c
Trevor-Miller <trevor.miller@logicalposition.com>
2023-03-03 08:16:18 -0800

    initial tutorial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const { App } = require("@slack/bolt");
+require("dotenv").config();
+const openai = require("openai");
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN, // add this
+  port: process.env.PORT || 3000,
+});
+
+// Listens to incoming messages that contain "hello"
+app.message("hello", async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say({
+    blocks: [
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `Hey there <@${message.user}>!`,
+        },
+        accessory: {
+          type: "button",
+          text: {
+            type: "plain_text",
+            text: "Click Me",
+          },
+          action_id: "button_click",
+        },
+      },
+    ],
+    text: `Hey there <@${message.user}>!`,
+  });
+});
+
+app.action("button_click", async ({ body, ack, say }) => {
+  // Acknowledge the action
+  await ack();
+  await say(`<@${body.user.id}> clicked the button`);
+});
+
+(async () => {
+  // Start your app
+  await app.start();
+
+  console.log("⚡️ Bolt app is running!");
+})();
